aefc1d86dc15d595944ce95119f9a6bea71ce77f
S26poste07\IPI <haddou.0014@gmail.com>
2018-11-09 14:50:26 +0100

    okeeey

---
+++ src/exam1/modulo.java
@@ -0,0 +1,8 @@
+package exam1;
+
+public class modulo {
+	public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("dfgdfgdfg");
+    }
+}
